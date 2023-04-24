5381f2133aca26348ea4832c4028a933e88120df
VincenzoCammilleri2 <cammilleriv2@gmail.com>
2023-04-24 10:02:56 -0400

    more styling and working on results card

--- CalorieCalc/views.py
+++ CalorieCalc/views.py
@@ -3,5 +3,10 @@ from django.shortcuts import render
 # Create your views here.
 
 
-def homePage(call):
-    return render(call, 'homePage.html')
+def homePage(request):
+    return render(request, 'homePage.html')
+
+
+def CalSumLogo(request):
+    image_url = '/static/images/logo.png'
+    return render(request, 'index.html', {'image_url': image_url})
